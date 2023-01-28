40d310508db83f6f61706ce104fd57fe3e82a592
Jeshua Leonard Pillay <73170733+Jeshuapillay@users.noreply.github.com>
2023-01-28 07:18:21 -0800

    Add files via upload

---
+++ Question 3 Assignment/WindowsApplication6/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(357, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 35)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Enter Sales"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(502, 20)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(245, 24)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Generate Sales Automatically"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(783, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 59)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Display Sales"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(889, 14)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 34)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.White
+        Me.RichTextBox1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.RichTextBox1.Location = New System.Drawing.Point(357, 104)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(607, 382)
+        Me.RichTextBox1.TabIndex = 4
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApplication6.My.Resources.Resources.inCollage_20200326_160046102
+        Me.ClientSize = New System.Drawing.Size(1003, 675)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Name = "Form1"
+        Me.Text = " ITI Hub regional sales"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Question 3 Assignment/WindowsApplication6/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+
+    Dim Columns As Integer = 3
+    Dim Rows As Integer = 5
+    Dim dblArraySales(Rows, Columns) As Integer
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        dblArraySales(0, 1) = 120000
+        dblArraySales(1, 1) = 190000
+        dblArraySales(2, 1) = 175000
+        dblArraySales(3, 1) = 188000
+        dblArraySales(4, 1) = 125000
+        dblArraySales(5, 1) = 163000
+
+        dblArraySales(0, 2) = 90000
+        dblArraySales(1, 2) = 85000
+        dblArraySales(2, 2) = 80000
+        dblArraySales(3, 2) = 83000
+        dblArraySales(4, 2) = 87000
+        dblArraySales(5, 2) = 80000
+
+        dblArraySales(0, 3) = 65000
+        dblArraySales(1, 3) = 64000
+        dblArraySales(2, 3) = 71000
+        dblArraySales(3, 3) = 67000
+        dblArraySales(4, 3) = 65000
+        dblArraySales(5, 3) = 64000
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim KwaZuluNatal As Integer = 0
+        Dim Gauteng As Integer = 0
+        Dim WesternCape As Integer = 0
+        Dim Sum As Integer = 0
+
+        RichTextBox1.Clear()
+
+        RichTextBox1.SelectionTabs = {50, 200, 300, 450}
+
+        RichTextBox1.AppendText("Month:" & vbTab & "Kwa-Zulu-Natal Sales:" & vbTab & "Gauteng Sales:" & vbTab & "Western Cape Sales:" & vbNewLine)
+        RichTextBox1.AppendText(vbNewLine)
+
+        For Month As Integer = 0 To Rows
+            RichTextBox1.AppendText(Month + 1 & vbTab)
+
+            For Region As Integer = 1 To Columns
+                RichTextBox1.AppendText(dblArraySales(Month, Region).ToString("C0") & vbTab)
+
+                If Region = 1 Then
+                    KwaZuluNatal += dblArraySales(Month, Region)
+                ElseIf Region = 2
+                    Gauteng += dblArraySales(Month, Region)
+                Else
+                    WesternCape += dblArraySales(Month, Region)
+                End If
+            Next Region
+
+            RichTextBox1.AppendText(vbNewLine)
+        Next Month
+
+        Sum = KwaZuluNatal + Gauteng + WesternCape
+
+        RichTextBox1.AppendText(vbNewLine & "The total amount that ITI Hub made in all 3 regions is:" & Sum.ToString("C0") & vbNewLine)
+
+        RichTextBox1.AppendText("KwaZuluNatal has contributed" & Math.Round(((KwaZuluNatal / Sum) * 100), 0) & "%" & "to the total." & vbNewLine)
+        RichTextBox1.AppendText("Gauteng has contributed" & Math.Round((Gauteng / Sum) * 100, 0) & "%" & "to the total." & vbNewLine)
+        RichTextBox1.AppendText("WesternCape has contributed" & Math.Round((Gauteng / Sum) * 100, 0) & "%" & "to the total." & vbNewLine)
+
+    End Sub
+End Class
